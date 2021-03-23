b68332cc87beca7aa3b4bff313a9855062cdab05
kuuhaku86 <yohan.ardiansyah90@gmail.com>
2021-03-23 20:46:03 +0700

    initialize backend

---
+++ Backend/src/test/java/com/tekber/elang/ELangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tekber.elang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
