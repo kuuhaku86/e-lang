216a49989d5e1c4bc6c9b9b91958701654d1aebf
kuuhaku86 <yohan.ardiansyah90@gmail.com>
2021-04-26 10:30:42 +0700

    adding bid activity's design

---
+++ Android/app/src/main/java/com/example/e_lang/BidActivity.java
@@ -0,0 +1,14 @@
+package com.example.e_lang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BidActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bid);
+    }
+}
--- Android/app/src/main/java/com/example/e_lang/MainActivity.java
+++ Android/app/src/main/java/com/example/e_lang/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.e_lang;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int[] arrayId = {
+                R.id.button2,
+                R.id.button3,
+                R.id.button4,
+        };
+
+        for (int id: arrayId) {
+            Button button = findViewById(id);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    MainActivity.this.onClick(v.getId());
+                }
+            });
+        }
+    }
+
+    public void onClick(int idView) {
+        switch (idView) {
+            case R.id.button2:
+                Intent intent = new Intent(MainActivity.this, BidActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.button3:
+                break;
+            case R.id.button4:
+                break;
+        }
     }
 }
